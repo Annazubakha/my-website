93c29d43f20d36a1e723a84d89b8ae0de7ee7af3
Annazubakha <korepovaanna@gmail.com>
2024-07-31 18:16:56 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,62 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      "sm-max": { max: "374.99px" },
+      md: "768px",
+      lg: "1440px",
+    },
+    fontFamily: {
+      montserrat: [
+        "ui-sans-serif",
+        "Montserrat",
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ],
+    },
+    extend: {
+      colors: {
+        "main-bg": "#212529",
+        white: "#ffff",
+        accent: "#9736bb",
+      },
+    },
+  },
+  corePlugins: {
+    container: false,
+  },
+  plugins: [
+    ({ addComponents }) => {
+      addComponents({
+        ".container": {
+          minWidth: "320px",
+          maxWidth: "375px",
+          marginLeft: "auto",
+          marginRight: "auto",
+          paddingLeft: "20px",
+          paddingRight: "20px",
+          "@screen md": {
+            paddingLeft: "32px",
+            paddingRight: "32px",
+            maxWidth: "768px",
+          },
+          "@screen lg": {
+            paddingLeft: "80px",
+            paddingRight: "80px",
+            maxWidth: "1440px",
+          },
+        },
+      });
+    },
+  ],
+};
